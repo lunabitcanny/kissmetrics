56cb61c537e66c2bf54010012c20a4492474f937
abhisekbitcanny <abhisek@bitcanny.com>
2017-08-11 12:27:54 +0530

    changes

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,11 +1,9 @@
 class TestsController < ApplicationController
   def sign_in
     session[:email] = 'luna@bitcanny.com'
-    render :json => 'I am in Signed In'
   end
 
   def visit
     session[:email] = 'anonymous'
-    render :json => 'I am anonymous'
   end
 end
